b47d605d9c938dc531e073a3f1830acd9da52a34
Milosh94 <milossm94@hotmail.com>
2017-06-15 21:19:23 +0200

    Windows agent initial, created as service

---
+++ WindowsAgent/Library.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics.Eventing.Reader;
+
+namespace WindowsServiceAgent
+{
+    public static class Library
+    {
+        public static void writeLog(string log, string fileName)
+        {
+            StreamWriter sw = null;
+
+            sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\" + fileName, true);
+            sw.WriteLine(log);
+            sw.Flush();
+            sw.Close();
+        }
+
+        public static void writeLastReadLogs(string line)
+        {
+
+        }
+
+        public static void readLastWrittenLogs(string eventFile)
+        {
+            //var lines = File.ReadLines("");
+            //foreach (var line in lines)
+        }
+
+        public static void readEventFile(object fileName)
+        {
+            string systemDirectory = Path.GetPathRoot(Environment.SystemDirectory);
+            string filePath = systemDirectory + @"Windows\System32\winevt\Logs\" + fileName.ToString();
+            DateTime lastModified = File.GetLastWriteTime(filePath);
+            using (var reader = new System.Diagnostics.Eventing.Reader.EventLogReader(filePath, PathType.FilePath))
+            {
+
+                EventRecord record;
+                while ((record = reader.ReadEvent()) != null)
+                {
+                    using (record)
+                    {
+                        string eventData = record.TimeCreated + record.LevelDisplayName + ":" + record.FormatDescription();
+                        writeLog(eventData, "Setup.txt");
+                        //Console.WriteLine("{0} {1}: {2}", record.TimeCreated, record.LevelDisplayName, record.FormatDescription());
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ WindowsAgent/SiemAgent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace WindowsServiceAgent
+{
+    public partial class SiemAgent : ServiceBase
+    {
+        Thread securityThread;
+        Thread systemThread;
+        Thread applicationThread;
+
+        public SiemAgent()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            securityThread = new Thread(Library.readEventFile);
+            securityThread.Start("Setup.evtx");
+            //systemThread = new Thread(Library.readEventFile);
+            //systemThread.Start("System.evtx");
+            //applicationThread = new Thread(Library.readEventFile);
+            //applicationThread.Start("Application.evtx");
+            Library.writeLog("upalio se servissssssssssss", "LogFile.txt");
+        }
+        
+        protected override void OnStop()
+        {
+            securityThread.Abort();
+            //systemThread.Abort();
+            //applicationThread.Abort();
+            Library.writeLog("servis ugasennnnnnnnnnnnnnn", "LogFile.txt");
+        }
+    }
+}
